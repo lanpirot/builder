42dc6786e2a81cdfc43b78feadcc9facfb9876bd
Alexander Boll <boll@informatik.hu-berlin.de>
2023-03-23 12:29:48 +0100

    first commit

---
+++ gathermodels.m
@@ -0,0 +1,21 @@
+function gathermodels()
+    modellist = [dir(fullfile(helper.models_path, '**\*.slx')); dir(fullfile(helper.models_path, '**\*.mdl'))];
+    project_info = tdfread(helper.project_info, 'tab');
+    fileID = fopen(helper.outfile, "w+");
+    for i = 1:length(modellist)
+        model = modellist(i);
+        folder = strsplit(model.folder, '\');
+        project_id = double(string(folder{8}));
+        project_info_row = find(project_info.path == project_id);
+        project_url = project_info.url(project_info_row,:);
+        project_url = split(project_url, ' ');
+        project_url = project_url{1};
+
+        model_url = string(model.folder) + filesep + model.name;
+        
+        row = replace(model_url + sprintf('\t') + project_url, "\", "/") + newline;
+        fprintf(fileID, row);
+    end
+    fclose(fileID);
+end
+        
---
+++ helper.m
@@ -0,0 +1,13 @@
+classdef helper
+    properties(Constant)
+        models_path = "C:\svns\simucomp2\models\SLNET_v1\SLNET";
+        tmp_models_path = "C:\svns\simucomp2\models\SLNET_v1\SLNET\SLNET_GitHub\7269901";
+        
+
+        project_info = "C:\svns\alex projects\builder\project_info.tsv";
+        outfile = "C:\svns\alex projects\builder\modellist.csv";
+    end
+    
+    methods(Static)
+    end
+end
