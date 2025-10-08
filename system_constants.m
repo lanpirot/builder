2bfb520d275b4e18fbf6a68b134b6f64266d8068
Alexander Boll <alexander.boll@unibe.ch>
2025-10-08 21:58:01 +0200

    added system_constants

--- system_constants.m
+++ system_constants.m
@@ -6,6 +6,6 @@ classdef system_constants
         synthed_models_path = "/home/lanpirot/data/builder/archive3";
         
         %models_path = "C:\svns\simucomp2\models\SLNET_v1\SLNET\";
-        %project_dir = "C:\svns\alex projects\builder\";
+        %project_dir = "C:\svns\XXXX-2 projects\builder\";
     end
 end
